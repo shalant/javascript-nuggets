f859ddff60faa394e942e9066e2723fd5cea5d59
Douglas Rosenberg <doug.rosenberg@gmail.com>
2022-02-05 21:45:15 -0600

    nice map refresher

---
+++ app.js
@@ -0,0 +1,28 @@
+// MAP Method
+const people = [
+  {
+    name: 'bob',
+    age: 20,
+    position: 'developer',
+  },
+  {
+    name: 'anna',
+    age: 25,
+    position: 'designer',
+  },
+  {
+    name: 'susy',
+    age: 30,
+    position: 'the boss',
+  },
+  {
+    name: 'john',
+    age: 26,
+    position: 'intern',
+  },
+];
+// Quokka.js - Extension
+// returns a new array
+// does not change the size of original array (unlike filter)
+// uses values from original array when making new one
+
---
+++ destructuring.js
@@ -0,0 +1,14 @@
+// faster/easier way to access/unpack variables from arrays, objects (later videos);
+
+const fruits = ['orange', 'banana', 'lemon'];
+const friends = ['john', 'peter', 'bob', 'anna', 'kelly'];
+
+const fruit1 = fruits[0];
+const fruit2 = fruits[1];
+const fruit3 = fruits[2];
+
+console.log(fruit1, fruit2, fruit3);
+
+const [john] = friends
+
+console.log(john);
