9a61dbec784cec4c08905c6a817b84a24ef08b5a
Douglas Rosenberg <doug.rosenberg@gmail.com>
2022-02-06 22:10:55 -0600

    dynamic object keys number 3

---
+++ dynamicObjectKeys.js
@@ -0,0 +1,43 @@
+// dot notation
+const person = {
+    name: 'john'
+};
+console.log(person.name);
+person.age = 25;
+console.log(person);
+
+// square bracket notation
+
+const items = {
+    'featured-items':['item1','item2']
+}
+
+console.log(items['featured-items']);
+console.log(person['name']);
+
+let appState = 'loading';
+appState = 'error';
+const keyName = 'computer'
+const app = {
+    [appState]:true
+}
+app[keyName] = 'apple'
+console.log(app);
+
+const state = {
+    loading:true,
+    name:'',
+    job:''
+}
+
+function updateState(key,value) {
+    state[key] = value
+}
+
+updateState('name', 'john')
+updateState('job', 'developer')
+updateState('loading', 'false')
+
+updateState('products', [])
+updateState('name', 'peter')
+console.log(state);
